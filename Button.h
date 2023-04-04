90b810c8f2d2b5e4fad425aee3d82d5e7df91a3b
piotrzegarek <piotr.zegarek.stud@pw.edu.pl>
2023-04-04 14:06:39 +0200

    update button states and colors

--- Button.h
+++ Button.h
@@ -1,13 +1,44 @@
 #ifndef BUTTON_H
 #define BUTTON_H
 
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <sstream>
+
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+enum button_states{BTN_IDLE = 0, BTN_HOVER, BTN_PRESSED };
+
 class Button
 {
 private:
+	short unsigned buttonState;
+
+	// Variables
+	sf::RectangleShape shape;
+	sf::Font* font;
+	sf::Text text;
+
+	sf::Color idleColor;
+	sf::Color hoverColor;
+	sf::Color activeColor;
+
+	// Functions
 
 public:
-	Button();
+	// Constructors/Destructors
+	Button(float x, float y, float width, float height,
+		sf::Font* font, std::string text,
+		sf::Color idleColor, sf::Color hoverColor, sf::Color activeColor);
 	~Button();
+
+	// Functions
+	void update(const sf::Vector2f mousePos);
+	void render(sf::RenderTarget* target);
+
 };
 
 #endif // !BUTTON_H
